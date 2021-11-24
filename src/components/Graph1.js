84ccf62a3d79830321a12445c7faa903d26ee2db
jogi6643 <jogi.amu@.com>
2021-11-24 17:26:19 +0530

    testing for graph high chart

--- src/components/Graph1.js
+++ src/components/Graph1.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 import axios from "axios";
-
+import {CategoryScale} from 'chart.js';
 const Graph1 = () => {
   const [chartData, setChartData] = useState({});
   const [employeeSalary, setEmployeeSalary] = useState([]);
   const [employeeAge, setEmployeeAge] = useState([]);
 
+
   const chart = () => {
     let empSal = [];
     let empAge = [];
-    axios
-      .get("http://dummy.restapiexample.com/api/v1/employees")
+    axios.get("http://dummy.restapiexample.com/api/v1/employees")
+    //   .get("https://financialmodelingprep.com/api/v3/financial-statement-symbol-lists?apikey=4756b0b52ab6fde26fa05b6a3aa1a723")
       .then(res => {
         console.log(res);
         for (const dataObj of res.data.data) {
@@ -40,6 +41,7 @@ const Graph1 = () => {
     chart();
   }, []);
   return (
+      
     <div className="App">
       <h1>Dyanmic Graph</h1>
       <div>
